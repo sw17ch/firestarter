0b43a87f296b6a70de24c3aed339467b57c14daf
John Van Enk <vanenkj@gmail.com>
2013-10-07 13:38:44 -0400

    Make it a little more obvious how to tune the width and height.

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@
 CFILES="plot.c forestfire.c util.c"
 CFLAGS="-Wall -Wextra -O3 "
 IMAGEMAGIC_FLAGS=`pkg-config --cflags --libs MagickCore`
+WIDTH="256"
+HEIGHT="256"
 
 PROG="forestfire"
 
 clang ${CFLAGS} ${CFILES} ${IMAGEMAGIC_FLAGS} \
-  -DWIDTH=800 \
-  -DHEIGHT=600 \
+  -DWIDTH=${WIDTH} \
+  -DHEIGHT=${HEIGHT} \
   -DDRAW_IMAGES \
   -DNOISY \
   -o ${PROG}
